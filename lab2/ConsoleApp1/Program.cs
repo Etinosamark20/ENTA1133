571c39d51e27c72b41373d961c8760d66d5c85bf
Etinosamark20 <etinosaibude100@gmail.com>
2025-09-12 18:05:17 -0700

    Submit lab2

---
+++ lab2/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Etinosaibudelab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I would convert DECIMAL to BINARY and HEXIDECIMAL");
+            Console.WriteLine("Decimal: 0 --> Binary: 0 --> Hex: 0000 ");
+            Console.WriteLine("Decimal: 1 --> Binary: 1 --> Hex: 0001 ");
+            Console.WriteLine("Decimal: 2 --> Binary: 10 --> Hex: 0002 ");
+            Console.WriteLine("Decimal: 4 --> Binary: 100 --> Hex: 0004 ");
+            Console.WriteLine("Decimal: 8 --> Binary: 1000 --> Hex: 0008 ");
+            Console.WriteLine("Decimal: 16 --> Binary: 10000 --> Hex: 0010 ");
+            Console.WriteLine("Decimal: 32 --> Binary: 100000 --> Hex: 0020 ");
+            Console.WriteLine("Decimal: 64 --> Binary: 1000000 --> Hex: 0040 ");
+            Console.WriteLine("Decimal: 100 --> Binary: 1100100 --> Hex: 0064 ");
+            Console.WriteLine("Decimal: 255 --> Binary: 11111111 --> Hex: 00FF ");
+            Console.WriteLine("Goodbye");
+            Console.WriteLine("Name: Mark");
+            Console.WriteLine("Date: 09-12-2025");
+
+
+        }
+    }
+}
